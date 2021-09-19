d6dfc3e3bcd89f803e7684c6e2cb276607b73f6f
litao <884058563@qq.com>
2021-09-19 22:02:55 +0800

    second commit

--- day06/src/demo01/Demo01Scanner.java
+++ day06/src/demo01/Demo01Scanner.java
@@ -6,5 +6,6 @@ public class Demo01Scanner {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println(sc.nextInt());
+        System.out.println("hello");
     }
 }
